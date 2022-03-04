6aefa6c840514324faee89b1c6ec7a49c6d0e911
CheonGyeHwan <cgh5531@naver.com>
2022-03-04 09:50:45 +0900

    express #1

--- helloweb-ex02/index.js
+++ helloweb-ex02/index.js
@@ -2,6 +2,8 @@ const http = require("http");
 const express = require("express");
 const path = require("path");
 const helloRouter = require("./routes/hello");
+const userRouter = require("./routes/user");
+const mainRouter = require("./routes/main");
 const port = 9090;
 
 // Application SetUp
@@ -12,12 +14,16 @@ const application = express()
                         .use(express.urlencoded({extended: true})) // application/x-www.form-url encoded
                         .use(express.json()) // application/json
                         // 3. view engine
+                        .set("views", path.join(__dirname, "/views"))
+                        .set("view engine", "ejs")
                         // 4. request router
                         .all("*", function(req, res, next) {
                             res.locals.req = req;
                             res.locals.res = res;
                             next();
                         })
+                        .use("/", mainRouter)
+                        .use("/user", userRouter)
                         .use("/hello", helloRouter);
 
 // Server SetUp
